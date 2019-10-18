f74cd32b650bd76f4f132bbcebb33777a554d3ee
Zekihan Azman <38537758+Zekihan@users.noreply.github.com>
2019-10-18 17:43:29 +0300

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -1,67 +1,78 @@
-# -*- coding: utf-8 -*-
+# -*- codin.g: utf-8 -*-
 """
 Created on Mon Sep 16 15:26:17 2019
 
 @author: Zekihan
 """
+import os
+import time
+import shutil
 
-path = "C:\\Users\\Zekihan\\Desktop\\logcat.txt"
-
-f = open(path,"r+", encoding="ISO-8859-1")
-logs = f.readlines()
-f.close()
-
-data = []
-for log in logs:
-    if "COMMAND_GET_CURRENT_BATTERY_LEVEL " in log:
-        data.append(log)
-
-data_l1 = []
-data_l2 = []
-data_l3 = []
-data_l4 = []
-data_l5 = []
-
-for i in data:
-    if i.split("Battery level : ")[1].split(" charging state : ")[0] == "1":
-        data_l1.append(int(i.split(" battVoltage : ")[1].split(",  Battery level : ")[0]))
-    if i.split("Battery level : ")[1].split(" charging state : ")[0] == "2":
-        data_l2.append(int(i.split(" battVoltage : ")[1].split(",  Battery level : ")[0]))
-    if i.split("Battery level : ")[1].split(" charging state : ")[0] == "3":
-        data_l3.append(int(i.split(" battVoltage : ")[1].split(",  Battery level : ")[0]))
-    if i.split("Battery level : ")[1].split(" charging state : ")[0] == "4":
-        data_l4.append(int(i.split(" battVoltage : ")[1].split(",  Battery level : ")[0]))
-    if i.split("Battery level : ")[1].split(" charging state : ")[0] == "5":
-        data_l5.append(int(i.split(" battVoltage : ")[1].split(",  Battery level : ")[0]))
-
-abs_max = max(data_l5)
-abs_low = min(data_l1)
-print(abs_low)
-print(abs_max)
-x = abs_max - abs_low
-
-for i in data_l1:
-    print(round((i - abs_low)/x*100))
+def create_shortcut(shortcut_path,target):
+    from win32com.client import Dispatch
     
-print("\n")
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(shortcut_path)
+    shortcut.Targetpath = target
+    shortcut.save()
 
-for i in data_l2:
-    print(round((i - abs_low)/x*100))
-        
-print("\n")
+DAYCOUNT = 14
 
-for i in data_l3:
-    print(round((i - abs_low)/x*100))
-    
-print("\n")
+save_path = "C:\\Users\\Zekihan\\Desktop\\güncel\\"
+shows_path = "D:\\Dizi\\"
 
-for i in data_l4:
-    print(round((i - abs_low)/x*100))
-    
-print("\n")
 
-for i in data_l5:
-    print(round((i - abs_low)/x*100))
+now = int(time.time())
+
+current = []
+
+shows = os.listdir(shows_path)
+for show in shows:
+    season = os.listdir(shows_path + show)
+    timex = 0
+    for i in season:
+        if timex < os.path.getmtime(shows_path+show+"\\"+i):
+            timex = int(os.path.getmtime(shows_path+show+"\\"+i))
+    if ((now-86400*DAYCOUNT) < timex):
+        current.append(show)
+
+for i in os.listdir(save_path):
+    p = save_path+"\\"+i
+    try:
+        if os.path.isfile(p):
+            os.remove(p)
+        else:    ## Show an error ##
+            shutil.rmtree(p)
+    except Exception as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))        
+
+for i in current:
+    create_shortcut(save_path + i + ".lnk", shows_path + i)    
+
+
+
+
+shows = os.listdir(shows_path)
+show = os.listdir(shows_path+shows[8])
+season = os.listdir(shows_path+shows[8]+"\\"+show[-1])
+for i in season:
+    timez =  os.path.getmtime(shows_path+shows[8]+"\\"+show[-1]+"\\"+i)
+    print(timez)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
